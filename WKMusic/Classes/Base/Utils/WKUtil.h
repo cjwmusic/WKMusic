6b563974227d854dccc5329ebe3ee0fc52029a40
Wu Kong <caijunwei1990@163.com>
2016-08-06 19:16:25 +0800

    添加工具类.

---
+++ WKMusic/Classes/Base/Utils/WKUtil.h
@@ -0,0 +1,13 @@
+//
+//  WKUtil.h
+//  WKMusic
+//
+//  Created by Wu Kong on 8/6/16.
+//  Copyright © 2016 wukong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WKUtil : NSObject
+
+@end
---
+++ WKMusic/Classes/Base/Utils/WKUtil.m
@@ -0,0 +1,13 @@
+//
+//  WKUtil.m
+//  WKMusic
+//
+//  Created by Wu Kong on 8/6/16.
+//  Copyright © 2016 wukong. All rights reserved.
+//
+
+#import "WKUtil.h"
+
+@implementation WKUtil
+
+@end
